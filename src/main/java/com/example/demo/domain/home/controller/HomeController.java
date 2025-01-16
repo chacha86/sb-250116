56c911ba4b0d413d8307e71a2309737b59d75443
chacha86 <taejincha0619@gmail.com>
2025-01-16 10:38:11 +0900

    json data

--- src/main/java/com/example/demo/domain/home/controller/HomeController.java
+++ src/main/java/com/example/demo/domain/home/controller/HomeController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+import java.util.Map;
+
 
 @Controller // @Controller 어노테이션을 붙여주면 스프링부트가 이 클래스를 컨트롤러로 인식하게 된다.
 public class HomeController {
@@ -29,4 +32,63 @@ public class HomeController {
         return age;
     }
 
+    @GetMapping("/byte")
+    @ResponseBody
+    public byte getByte() {
+        return 1;
+    }
+
+    @GetMapping("/short")
+    @ResponseBody
+    public short getShort() {
+        return 1;
+    }
+
+    @GetMapping("/long")
+    @ResponseBody
+    public long getLong() {
+        return 1;
+    }
+
+    @GetMapping("/float")
+    @ResponseBody
+    public float getFloat() {
+        return 3.14f;
+    }
+
+    @GetMapping("/double")
+    @ResponseBody
+    public double getDouble() {
+        return 3.14;
+    }
+
+    @GetMapping("/char")
+    @ResponseBody
+    public char getChar() {
+        return '안';
+    }
+
+    @GetMapping("/boolean")
+    @ResponseBody
+    public boolean getBoolean() {
+        return true;
+    }
+
+    @GetMapping("/array")
+    @ResponseBody
+    public String[] getArray() {
+        return new String[] {"a", "b", "c"};
+    }
+
+    @GetMapping("/list")
+    @ResponseBody
+    public List<String> getList() {
+        return List.of("a", "b", "c");
+    }
+
+    @GetMapping("/map")
+    @ResponseBody
+    public Map<String, String> getMap() {
+        return Map.of("k1", "v1", "k2", "v2");
+    }
 }
