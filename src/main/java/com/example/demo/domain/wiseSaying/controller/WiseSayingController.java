6d3e6918d1f7448710e401535e66ed52584c067d
chacha86 <taejincha0619@gmail.com>
2025-01-16 14:22:41 +0900

    modify

--- src/main/java/com/example/demo/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/example/demo/domain/wiseSaying/controller/WiseSayingController.java
@@ -36,4 +36,9 @@ public class WiseSayingController {
         return wiseSayingService.deleteById(id);
     }
 
+    @GetMapping("/wiseSayings/{id}/modify")
+    public WiseSaying modifyItem(@PathVariable int id, String content, String author) {
+        return wiseSayingService.modify(id, content, author);
+    }
+
 }
