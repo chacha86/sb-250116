d14116cb7d683a7b41c1a6cb36dce8481e8c8490
chacha86 <taejincha0619@gmail.com>
2025-01-16 10:09:20 +0900

    annotation meaning

--- src/main/java/com/example/demo/domain/home/controller/HomeController.java
+++ src/main/java/com/example/demo/domain/home/controller/HomeController.java
@@ -4,11 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+
+@Controller // @Controller 어노테이션을 붙여주면 스프링부트가 이 클래스를 컨트롤러로 인식하게 된다.
 public class HomeController {
 
-    @GetMapping("/")
-    @ResponseBody
+    @GetMapping("/") // @GetMapping 어노테이션을 붙여주면 / 경로로 들어왔을 때 이 메소드가 실행된다.
+    @ResponseBody // @ResponseBody 어노테이션을 붙여주면 메서드의 리턴값을 브라우저에 보여준다.
     public String home() {
         return "hello";
     }
